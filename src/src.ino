3e236413b8d404cccf8ff3feeba2477057c1616c
Michael Wu <michaelmwu@gmail.com>
2013-02-23 18:22:14 -0800

    counts hits

--- src/src.ino
+++ src/src.ino
@@ -1,10 +1,16 @@
 #define ACCEL_WINDOW 3
 #define ACCEL_SAMPLES 4
-int accelSamples[ACCEL_SAMPLES];
+unsigned long accelSamples[ACCEL_SAMPLES];
 int accelOffset = 0;
-const int accelSensor = A0; // the accelerometer is connected to analog pin 0
-const int kickThreshold = 12;
-const int fallThreshold = 7;
+const int accelSensorX = A0; // the accelerometer is connected to analog pin 3, 6, 7
+const int accelSensorY = A1;
+const int accelSensorZ = A2;
+const int ACCEL_MIDDLE = 512;
+
+const unsigned long kickThreshold = 1000000;
+const unsigned long fallThreshold = 5000;
+const unsigned long FALL_TIME = 100;
+const unsigned long KICKSTATIONARY_TIME = 200;
 
 const int STATE_STATIONARY = 0;
 const int STATE_KICK = 1;
@@ -12,8 +18,10 @@ const int STATE_FALL = 2;
 int hacky_state = STATE_STATIONARY;
 unsigned long lastStateTime;
 
+unsigned long hits = 0;
+
 // Time stationary until we assume the hacksack is done
-const int STATIONARY_TIME = 3000;
+const unsigned long STATIONARY_TIME = 3000;
 
 // Setup code
 void setup() {
@@ -27,6 +35,7 @@ void reset() {
   // Zero out accelerometer data array
   memset(accelSamples,0,sizeof(accelSamples));
   lastStateTime = millis();
+  hits = 0;
 }
 
 // Main
@@ -34,13 +43,34 @@ void loop() {
   delay(50);
   recordAccelSample();
   
-  int reading = accelReading();
-  Serial.println(reading);
+  unsigned long reading = accelReading();
+//  Serial.print("weighted: ");
+//  Serial.println(reading);
+  stateMachine(reading);
+}
+
+int rebase(int sample) {
+  sample -= ACCEL_MIDDLE;
+  return sample * sample;
 }
 
 // Add it to the accelerometer buffers
 void recordAccelSample() {
-  int sample = analogRead(accelSensor);
+  int xsample = analogRead(accelSensorX);
+  int ysample = analogRead(accelSensorY);
+  int zsample = analogRead(accelSensorZ);
+  
+//  Serial.print("X: ");
+//  Serial.print(xsample);
+//  Serial.print(" Y: ");
+//  Serial.print(ysample);
+//  Serial.print(" Z: ");
+//  Serial.print(zsample);
+  
+  unsigned long sample = rebase(xsample)+rebase(ysample)+rebase(zsample);
+//  Serial.print("final: ");
+//  Serial.println(sample);
+  
   accelSamples[accelOffset] = sample;
 
   accelOffset++;
@@ -50,11 +80,11 @@ void recordAccelSample() {
 }
 
 // Get a weighted accelerometer reading
-int accelReading() {
-  int total = 0;
+unsigned long accelReading() {
+  unsigned long total = 0;
   
   for(int i = 0; i < ACCEL_WINDOW; i++) {
-    int offset = accelOffset - i - 1;
+    int offset = (accelOffset - i - 1 + ACCEL_SAMPLES) % ACCEL_SAMPLES;
     
     total += accelSamples[offset];
   }
@@ -62,13 +92,78 @@ int accelReading() {
   return total / ACCEL_WINDOW;
 }
 
-void stateMachine(int reading) {
+void stateMachine(unsigned long reading) {
   unsigned long since_last = millis() - lastStateTime;
+  int next_state = hacky_state;
+  
+//  Serial.print("STATE: ");
+//  if(hacky_state == STATE_STATIONARY) {
+//    Serial.println("STATIONARY");
+//  }
+//  else if(hacky_state == STATE_FALL) {
+//    Serial.println("FALL");
+//  } else if(hacky_state == STATE_KICK) {
+//    Serial.println("KICK");
+//  }
   
   if(hacky_state == STATE_STATIONARY) {
+    if(reading > kickThreshold) {
+      next_state = STATE_KICK;
+      hits += 1;
+      Serial.print("hits: ");
+      Serial.println(hits);
+    }
+    else if(reading < fallThreshold) {
+      next_state = STATE_FALL && since_last >= FALL_TIME;
+    }
+    
+    if(since_last >= STATIONARY_TIME) {
+      Serial.println("RESET");
+      reset();
+    }
   }
   else if(hacky_state == STATE_KICK) {
+    if(reading < fallThreshold && since_last >= FALL_TIME) {
+      next_state = STATE_FALL;
+    }
+    else if(reading < kickThreshold && since_last >= KICKSTATIONARY_TIME) {
+      next_state = STATE_STATIONARY;
+    }
   }
   else if(hacky_state == STATE_FALL) {
+    if(reading > kickThreshold) {
+      next_state = STATE_KICK;
+      hits += 1;
+      Serial.print("hits: ");
+      Serial.println(hits);
+    }
+    else if(reading > fallThreshold && since_last >= KICKSTATIONARY_TIME) {
+      next_state = STATE_STATIONARY;
+    }
+  }
+
+  if(hacky_state != next_state) {
+    Serial.print("STATE: ");
+    if(hacky_state == STATE_STATIONARY) {
+      Serial.println("STATIONARY");
+    }
+    else if(hacky_state == STATE_FALL) {
+      Serial.println("FALL");
+    } else if(hacky_state == STATE_KICK) {
+      Serial.println("KICK");
+    }
+    
+    Serial.print("NEXT STATE: ");
+    if(next_state == STATE_STATIONARY) {
+      Serial.println("STATIONARY");
+    }
+    else if(next_state == STATE_FALL) {
+      Serial.println("FALL");
+    } else if(next_state == STATE_KICK) {
+      Serial.println("KICK");
+    }
+    
+    lastStateTime = millis();
+    hacky_state = next_state;
   }
 }
